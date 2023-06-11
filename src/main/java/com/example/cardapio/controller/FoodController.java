97adb8c51e620508d4320577edff3ceceab22fd1
Wendell Marcos <wendell.marcos.code@gmail.com>
2023-06-12 00:17:32 +0100

    logicas e classes do cardapio sendo idealizado

---
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -0,0 +1,24 @@
+package com.example.cardapio.controller;
+
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    public List<Food> getAll(){
+
+        List<Food> foodList = repository.findAll();
+        return foodList;
+    }
+}
